76b2ba8279b1bdc8cea23698cee65006d23f7f70
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-04-05 21:39:05 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Statistic.isGreenLight = false;
-        int[] speedOfPlayer = new int[3];
-        speedOfPlayer[0] = 0;
-        speedOfPlayer[1] = 3;
-        speedOfPlayer[2] = 2;
-        int numberOfDropouts = Statistic.numberOfDropouts(speedOfPlayer);
-        System.out.println("Количество выбывших игроков " + numberOfDropouts);
-        int[] speedsOfDropped = Statistic.speedsOfDropped(speedOfPlayer);
-        System.out.println("Скорость выбывающих " + Arrays.toString(speedsOfDropped));
-        int[] speedsOfNotDropped = Statistic.speedsOfNotDropped(speedOfPlayer);
-        System.out.println("Скорость не выбывающих " + Arrays.toString(speedsOfNotDropped));
-        int[] speedsOfNotDropped = Statistic.MAX_SPEED(speedOfPlayer);
-        System.out.println("Скорости игроков выбывшие по превышению максимальной скорости " + Arrays.toString(MAX_SPEED));
-    }
-}
+        public static void main(String[] args) {
+                Statistic.isGreenLight = false;
+                int[] speedOfPlayer = new int[3];
+                speedOfPlayer[0] = 0;
+                speedOfPlayer[1] = 3;
+                speedOfPlayer[2] = 2;
+                int numberOfDropouts = Statistic.numberOfDropouts(speedOfPlayer);
+                System.out.println("Количество выбывших игроков " + numberOfDropouts);
+                int[] speedsOfDropped = Statistic.speedsOfDropped(speedOfPlayer);
+                System.out.println("Скорость выбывающих " + Arrays.toString(Statistic.speedsOfDropped(speedOfPlayer)));
+                int[] speedsOfNotDropped = Statistic.speedsOfNotDropped(speedOfPlayer);
+                System.out.println("Скорость не выбывающих " + Arrays.toString(Statistic.speedsOfNotDropped(speedOfPlayer)));
+                int[] MAX_SPEED = Statistic.MAX_SPEED(speedOfPlayer);
+                System.out.println("Скорости игроков выбывшие по превышению максимальной скорости " + Arrays.toString(Statistic.MAX_SPEED(speedOfPlayer)));
        
+        }
+
+}
